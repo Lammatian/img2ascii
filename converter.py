2911ea41bd77bc41494e98f5a0db2dcac5ed1e3e
Lammatian <mateusz.parafinski@gmail.com>
2018-08-04 14:01:13 +0200

    Rewrote finding best match as function, some images for testing

--- converter.py
+++ converter.py
@@ -17,20 +17,26 @@ def cos_similarity(a, b):
 def show_binary_image(a):
     Image.frombytes(mode='1', size=a.shape[::-1], data=np.packbits(a, axis=1)).show()
 
-arr = np.random.randint(2, size=(39, 20))
-show_binary_image(arr)
-best_img = None
-best_similarity = 0
+def find_best_match(imagepath):
+    binary_image = convert_to_binary(imagepath)
 
-for i in range(32, 127):
-    asc = convert_to_binary("img/" + str(i) + ".png")
-    #arr = convert_to_binary("img/100.png")
-    similarity = cos_similarity(asc, arr)
+    best_match = None
+    best_similarity = 0
 
-    if similarity > best_similarity:
-        best_img = asc
-        best_similarity = similarity
+    for i in range(32, 127):
+        asc = convert_to_binary("img/" + str(i) + ".png")
+        similarity = cos_similarity(asc, binary_image)
 
-    print("{}: similarity {}".format(i, similarity))
+        if similarity > best_similarity:
+            best_match = asc
+            best_similarity = similarity
+        
+        print("{}: similarity {}".format(i, similarity))
+    
+    show_binary_image(best_match)
+    return best_match
 
-show_binary_image(best_img)
+#arr = np.random.randint(2, size=(39, 20))
+#show_binary_image(arr)
+
+find_best_match("lettery2.jpg")

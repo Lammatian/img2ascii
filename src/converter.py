5f206f7f5dcc900cdbf74c71e21ec18baffec683
Lammatian <mateusz.parafinski@gmail.com>
2018-08-05 17:04:20 +0200

    Implemented different similarity methods for testing

--- src/converter.py
+++ src/converter.py
@@ -2,8 +2,6 @@ from PIL import Image
 import numpy as np
 from scipy import spatial
 
-# TODO: Do something about 32.png - zero vector, no similarity!
-# TODO: Test different types of similarity
 def convert_to_binary(imagepath):
     img = Image.open(imagepath).resize((20, 39))
     arr = np.array(img.convert('L'))
@@ -11,8 +9,26 @@ def convert_to_binary(imagepath):
     arr[arr > 128] = 1
     return arr
 
+"""
+Similarity treating pictures as vectors and calculating the angle between them
+"""
+# TODO: What about 0 vector?
 def cos_similarity(a, b):
     return 1 - spatial.distance.cosine(a.ravel(), b.ravel())
+
+"""
+Similarity calculating how many squares have the same colour (value)
+"""
+# TODO: Black and white shouldn't have the same significance, shape is more important
+def binary_similarity(a, b):
+    return a.size - np.sum(np.bitwise_xor(a, b).ravel())
+
+"""
+Similarity based on how many white (value 1) squares match
+"""
+# TODO: If a picture is totally white, it will always be the best
+def white_similarity(a, b):
+    return np.sum((a*b).ravel())
 
 def show_binary_image(a):
     Image.frombytes(mode='1', size=a.shape[::-1], data=np.packbits(a, axis=1)).show()
@@ -20,23 +36,25 @@ def show_binary_image(a):
 def find_best_match(imagepath):
     binary_image = convert_to_binary(imagepath)
 
-    best_match = None
-    best_similarity = 0
+    best_match_binary = None
+    best_similarity_binary = 0
+    best_match_cos = None
+    best_similarity_cos = 0
 
     for i in range(32, 127):
         asc = convert_to_binary("img/" + str(i) + ".png")
-        similarity = cos_similarity(asc, binary_image)
+        similarity_c = cos_similarity(asc, binary_image)
+        similarity_b = binary_similarity(asc, binary_image)
 
-        if similarity > best_similarity:
-            best_match = asc
-            best_similarity = similarity
+        if similarity_c > best_similarity_cos:
+            best_match_cos = asc
+            best_similarity_cos = similarity_c
+        if similarity_b > best_similarity_binary:
+            best_match_binary = asc 
+            best_similarity_binary = similarity_b
         
-        print("{}: similarity {}".format(i, similarity))
+        print("{}: cosine similarity {} | binary similarity {}".format(i, similarity_c, similarity_b))
     
-    show_binary_image(best_match)
-    return best_match
-
-#arr = np.random.randint(2, size=(39, 20))
-#show_binary_image(arr)
-
-find_best_match("lettery2.jpg")
+    show_binary_image(best_match_cos)
+    show_binary_image(best_match_binary)
+    return best_match_cos
